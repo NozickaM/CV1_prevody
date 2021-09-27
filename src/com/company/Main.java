14750388e0c40a983d45f7fc1f553b12a091c3fb
Miroslav Nožička <st67079@upce.cz>
2021-09-27 15:37:09 +0200

    Format code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,15 +9,15 @@ public class Main {
 
         System.out.print("Gallons: ");
         double gallons = input.nextDouble();
-        System.out.println("Litres: "+gallons*3.7854);
+        System.out.println("Litres: " + gallons * 3.7854);
 
         System.out.print("Kilos on Earth: ");
         double kilos = input.nextDouble();
-        System.out.println("Kilos on the Moon: "+kilos*0.17);
+        System.out.println("Kilos on the Moon: " + kilos * 0.17);
 
         System.out.print("Circle radius: ");
         double radius = input.nextDouble();
-        System.out.println("Circle circumference: "+2*Math.PI*radius);
-        System.out.println("Circle area: "+Math.PI*Math.pow(radius,2));
+        System.out.println("Circle circumference: " + 2 * Math.PI * radius);
+        System.out.println("Circle area: " + Math.PI * Math.pow(radius, 2));
     }
 }
